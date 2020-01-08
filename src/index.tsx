97546cf5674f481a667c8cce676654c3c77d55bb
Hitoshi Watanabe <h_watanabe@HitoshinoMacBook-Pro.local>
2020-01-08 10:52:51 +0900

    JSXで書き換える

--- src/index.tsx
+++ src/index.tsx
@@ -3,53 +3,65 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Square extends React.Component {
-    render() {
-      return (
-        React.createElement("button", { className: "square" }));
-    }}
-  
-  class Board extends React.Component {
-    renderSquare(i: number) {
-      return React.createElement(Square, null);
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        React.createElement("div", null,
-        React.createElement("div", { className: "status" }, status),
-        React.createElement("div", { className: "board-row" },
-        this.renderSquare(0),
-        this.renderSquare(1),
-        this.renderSquare(2)),
-  
-        React.createElement("div", { className: "board-row" },
-        this.renderSquare(3),
-        this.renderSquare(4),
-        this.renderSquare(5)),
-  
-        React.createElement("div", { className: "board-row" },
-        this.renderSquare(6),
-        this.renderSquare(7),
-        this.renderSquare(8))));
-    }}
-  
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        React.createElement("div", { className: "game" },
-        React.createElement("div", { className: "game-board" },
-        React.createElement(Board, null)),
-  
-        React.createElement("div", { className: "game-info" },
-        React.createElement("div", null),
-        React.createElement("ol", null))));
-    }}
-  
-  // ========================================
-  
-  ReactDOM.render(
-  React.createElement(Game, null),
-  document.getElementById('root'));
+  render() {
+    return(
+      <button className="square">
+      </button>
+    ) 
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i: number) {
+    return <Square />
+  }
+
+  render() {
+    const status = "Next player: X"
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div></div>
+          <ol></ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+)
