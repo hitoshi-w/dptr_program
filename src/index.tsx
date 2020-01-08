538756315f59a119000a1ced911ea59914da101d
Hitoshi Watanabe <h_watanabe@HitoshinoMacBook-Pro.local>
2020-01-08 14:41:05 +0900

    ゲーム勝者の判定する機能を追加

--- src/index.tsx
+++ src/index.tsx
@@ -26,6 +26,7 @@ interface BoardPropsInterface {
 interface BoardStateInterface {
   squares: Array<string>
   xIsNext: boolean
+  winner: string
 }
 
 class Board extends React.Component<BoardPropsInterface, BoardStateInterface>{
@@ -34,12 +35,19 @@ class Board extends React.Component<BoardPropsInterface, BoardStateInterface>{
     this.state = {
       squares: Array(9).fill(""),
       xIsNext: true,
+      winner: ""
     }
   }
 
   handleClick(i: number) {
+    const winner = calculateWinner(this.state.squares)
+    if (winner || this.state.squares[i]) {
+      return;
+    }
+
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O'
+
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext
@@ -54,7 +62,13 @@ class Board extends React.Component<BoardPropsInterface, BoardStateInterface>{
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner 
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
 
     return (
       <div>
@@ -105,3 +119,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 )
+
+function calculateWinner(squares: Array<string>) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    console.log(squares[a]);
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return ""
+}
