3e22113aec13eeb457ca414c65edcc56f8184dd8
Hitoshi Watanabe <h_watanabe@HitoshinoMacBook-Pro.local>
2020-01-09 10:53:05 +0900

    タイムトラベル機能の実装

--- src/index.tsx
+++ src/index.tsx
@@ -68,6 +68,7 @@ interface GameStateInterface {
     squares: Array<string>
 　}>
   xIsNext: boolean
+  stepNumber: number
 }
 
 class Game extends React.Component<GamePropsInterface, GameStateInterface> {
@@ -77,14 +78,15 @@ class Game extends React.Component<GamePropsInterface, GameStateInterface> {
       history: [{
         squares: Array(9).fill("")
       }],
+      stepNumber: 0,
       xIsNext: true
     }
   }
 
   handleClick(i: number) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber+1)
     const current = history[history.length - 1]
-    const squares = current.squares.slice()
+    const squares = current.squares.slice() //配列のコピーを作成
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -93,14 +95,33 @@ class Game extends React.Component<GamePropsInterface, GameStateInterface> {
       history: history.concat([{
         squares: squares
       }]),
-      xIsNext: !this.state.xIsNext   
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext 
+    })
+  }
+
+  jumpTo(step: number) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     })
   }
 
   render() {
-    const history = this.state.history
-    const current = history[history.length - 1]
-    const winner = calculateWinner(current.squares)
+    const history = this.state.history   
+    const current = history[this.state.stepNumber] //{squares: Array(9)}
+    const winner = calculateWinner(current.squares) //current.squares ["", "", "X", "", "", "", "", "", ""]、クリック時の情報がこの配列に記録される
+
+    const moves = history.map((step, move) => { 
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start'
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
 
     let status
     if (winner) {
@@ -119,7 +140,7 @@ class Game extends React.Component<GamePropsInterface, GameStateInterface> {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol></ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     )
@@ -153,7 +174,6 @@ function calculateWinner(squares: Array<string>) {
   ]
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
-    console.log(squares[a]);
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]
     }
