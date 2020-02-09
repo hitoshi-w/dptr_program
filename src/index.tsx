07d56120b0b9c57b190934a6c802e9488d29ebb6
Hitoshi Watanabe <h_watanabe@HitoshinoMacBook-Pro.local>
2020-02-09 11:09:17 +0900

    Configure firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'index.css';
 
+import 'firebase/firestore';
+import fb from 'config/fbConfig';
+
+import App from 'App';
+import * as serviceWorker from 'serviceWorker';
+
+export const db = fb.firestore();
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
-
