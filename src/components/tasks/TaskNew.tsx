80d9dc16be3b8c631b0f0c92a4a442313e310212
Hitoshi Watanabe <h_watanabe@HitoshinoMacBook-Pro.local>
2020-02-14 18:25:29 +0900

    Layout the form to create a task

--- src/components/tasks/TaskNew.tsx
+++ src/components/tasks/TaskNew.tsx
@@ -1,30 +1,78 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+import {
+  TextField,
+  Button,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+} from '@material-ui/core';
+import styled from 'styled-components';
+
 export type TaskForm = {
   title: string;
+  staff: string;
+  priority: string;
 };
 
 const TaskNew: React.FC = () => {
   const { register, handleSubmit } = useForm<TaskForm>();
-  const onSubmit = handleSubmit(({ title }) => {
-    console.log(title);
+  const onSubmit = handleSubmit(({ title, staff, priority }) => {
+    console.log(title, staff, priority);
   });
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <h5>タスク作成</h5>
-        <div>
-          <label htmlFor="title">タスク名</label>
-          <input type="text" name="title" ref={register} />
-        </div>
-        <div>
-          <button type="submit">さくせい</button>
-        </div>
-      </form>
-    </div>
+    <Forma onSubmit={onSubmit}>
+      <h2>タスク作成</h2>
+      <TextField inputRef={register} label="タスク" name="title" />
+      <TextField inputRef={register} label="担当者" name="staff" />
+      <FormControl component="fieldset">
+        <FormLabel component="legend">優先度</FormLabel>
+        <RadioGroup
+          aria-label="priority"
+          name="priority-radio"
+          defaultValue="lowPriority"
+          row
+        >
+          <FormControlLabel
+            value="highPriority"
+            control={
+              <Radio color="primary" inputRef={register} name="priority" />
+            }
+            label="高"
+            labelPlacement="start"
+          />
+          <FormControlLabel
+            value="lowPriority"
+            control={
+              <Radio color="primary" inputRef={register} name="priority" />
+            }
+            label="低"
+            labelPlacement="start"
+          />
+        </RadioGroup>
+      </FormControl>
+      <Button variant="contained" color="primary" type="submit">
+        作成
+      </Button>
+    </Forma>
   );
 };
+
+const Forma = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 40rem;
+  height: 45rem;
+  padding: 2rem;
+  h2 {
+    align-self: center;
+    margin: 0;
+  }
+`;
 
 export default TaskNew;
